f1686c11f7fe033aaa64d570f6df839eb6fe1b78
dinakaranthiru <dinakarant09@gmail.com>
2025-05-15 15:18:29 +0530

    Updated Context API

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,7 +14,9 @@ export const AppContextProvider = (props) => {
 
   const getAuthState = async () => {
     try {
-      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth",{
+        withCredentials:true
+      });
       if (data.success) {
         setIsLoggedin(true);
         getUserData()
@@ -26,7 +28,9 @@ export const AppContextProvider = (props) => {
 
   const getUserData = async () => {
     try {
-      const { data } = await axios.get(backendUrl + "/api/user/data");
+      const { data } = await axios.get(backendUrl + "/api/user/data",{
+        withCredentials:true
+      });
       data.success ? setUserData(data.userData) : toast.error(data.message);
     } catch (error) {
       toast.error(error.message || "Failed to fetch user data");
