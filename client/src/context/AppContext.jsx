52cf40c4312a17cb859d5db12ea86003901bff28
dinakaranthiru <dinakarant09@gmail.com>
2025-05-15 16:19:26 +0530

    updated header components

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,21 +5,20 @@ import { toast } from "react-toastify";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-
   axios.defaults.withCredentials = true;
-  
+
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(null);
 
   const getAuthState = async () => {
     try {
-      const { data } = await axios.get(backendUrl + "/api/auth/is-auth",{
-        withCredentials:true
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth", {
+        withCredentials: true,
       });
       if (data.success) {
         setIsLoggedin(true);
-        getUserData()
+        getUserData();
       }
     } catch (error) {
       toast.error(error.message);
@@ -28,8 +27,8 @@ export const AppContextProvider = (props) => {
 
   const getUserData = async () => {
     try {
-      const { data } = await axios.get(backendUrl + "/api/user/data",{
-        withCredentials:true
+      const { data } = await axios.get(backendUrl + "/api/user/data", {
+        withCredentials: true,
       });
       data.success ? setUserData(data.userData) : toast.error(data.message);
     } catch (error) {
@@ -39,8 +38,7 @@ export const AppContextProvider = (props) => {
 
   useEffect(() => {
     getAuthState();
-
-  },[])
+  }, []);
 
   const value = {
     backendUrl,
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Header from "../components/HEader";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
